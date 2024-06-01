b1a5f2f233b4f34f8b5c23af8e075d37d1673454
preetsuthar17 <preetsutharxd@gmail.com>
2024-06-01 19:50:32 +0530

    feat: Update Home page to include About, Contact, Intro, and Projects components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,1 +1,15 @@
-export default function Home() {}
+import { About } from "@/components/About";
+import { Contact } from "@/components/Contact";
+import { Intro } from "@/components/Intro";
+import { Projects } from "@/components/Projects";
+
+export default function Home() {
+  return (
+    <>
+      <Intro />
+      <About />
+      <Projects />
+      <Contact />
+    </>
+  );
+}
