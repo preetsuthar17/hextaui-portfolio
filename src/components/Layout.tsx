05aa9504f5e1c4a626bfba31b6af560ee995b813
preetsuthar17 <preetsutharxd@gmail.com>
2024-06-01 19:47:53 +0530

    chore: Update Layout component to include Navbar and Footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import { GeistSans } from "geist/font/sans";
+import { Navbar } from "./Navbar";
+import { Footer } from "./Footer";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <main >{children}</main>
+      <Navbar />
+      <main className={`${GeistSans.className} min-h-[100vh]`}>{children}</main>
+      <Footer />
     </>
   );
 };
